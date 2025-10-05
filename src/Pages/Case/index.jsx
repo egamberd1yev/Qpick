d9286c41b0161126e948c0213e8a79ab8d758399
egamberd1yev <jorabekegamberdiyev12@gmail.com>
2025-10-05 15:20:20 +0500

    Case section start

---
+++ src/Pages/Case/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Container, Box, Text } from '@chakra-ui/react'
+
+const Case = () => {
+  return (
+    <section style={{backgroundColor: '#EAEAEA', height: '80vh'}}>
+      <Container>
+      <img src="src/Pages/Case/index.jsx src/assets/transparent.jpg" alt="" />
+      </Container>
+    </section>
+  )
+}
+
+export default Case
--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import Navbar from '../Navbar'
 import About from '../About'
-
+import Case from '../Case'
 const Home = () => {
   return (
     <>
       <Navbar />
       <About />
+      <Case />
     </>
   )
-}
+} 
 
 export default Home
